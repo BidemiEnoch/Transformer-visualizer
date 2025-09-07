2bb37a3647b721e89cf98ab607f715adf6ea1859
Abidemi Koledoye <bids416007@gmail.com>
2025-09-06 23:47:30 -0500

    cleaning up stuff

--- web-server/server.js
+++ web-server/server.js
@@ -15,6 +15,7 @@ const PORT = 3000;
 const dummy_input = ["<bos>","it", "is", "all", "exagerrated","<eos>"];
 const segments = util.getTokenColors(dummy_input);
 
+
 app.get('/', (req, res) => {
     res.render("home", { segments });
 });
