9161c180371cb040eb09bb3d6afaf37fdc0b9177
Abidemi Koledoye <bids416007@gmail.com>
2025-10-15 20:24:34 -0500

    minor fixes

--- web-server/server.js
+++ web-server/server.js
@@ -12,6 +12,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = 3000;
 
+
 const dummy_input = ["<bos>","it", "is", "not", "exagerrated","<eos>"];
 const segments = util.getTokenColors(dummy_input);
 
@@ -19,10 +20,8 @@ app.get('/', (req, res) => {
     res.render("home", { segments });
 });
 
-
 app.post('/submit', async (req, res) => {
     const data = req.body;
-
     res.render("home", { segments });
     try {
         const response = await axios.post(FLASK_SERVER, data);
